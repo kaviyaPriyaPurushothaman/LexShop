6a0d74234546d7f1ea20ca3ff60e9cefedd43c56
KaviyaPriyaPurushothaman <droptokaviyapriya@gmail.com>
2021-01-08 11:07:52 +0100

    Product Model and InMemory Product Repository Built

---
+++ LexShop.Core/Models/Products.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexShop.Core.Models
+{
+    public class Product
+    {
+        public String Id { get; set;}
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public String Name { get; set; }
+        public String Description { get; set; }
+        [Range(0,1000)]
+        public Decimal Price { get; set; }
+        public String Category { get; set; }
+        public String Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            
+        }
+
+
+
+
+
+    }
+}
