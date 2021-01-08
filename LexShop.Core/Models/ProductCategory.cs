95565f24f46a2ae11c64d62cea9bf96c24befbbb
KaviyaPriyaPurushothaman <droptokaviyapriya@gmail.com>
2021-01-08 17:24:02 +0100

    Product grouping built and test

---
+++ LexShop.Core/Models/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexShop.Core.Models
+{
+    public class ProductCategory
+    {
+        public String Id { get; set; }
+        public String Category { get; set; }
+
+        public ProductCategory()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
