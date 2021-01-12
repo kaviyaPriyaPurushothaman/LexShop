b37f96fbc44b9835e84cc72e4790caf514828085
KaviyaPriyaPurushothaman <droptokaviyapriya@gmail.com>
2021-01-12 16:40:41 +0100

    Add Entity Framework

---
+++ LexShop.DataAccessSQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LexShop.Core.Models;
+
+namespace LexShop.DataAccessSQL
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories{ get; set; }
+
+    }
+}
